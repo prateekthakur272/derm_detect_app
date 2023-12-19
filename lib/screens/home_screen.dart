fb384a618eb2eb412b88e43722a61ace102801f1
Prateek Thakur <prateekthakur272@gmail.com>
2023-12-20 01:45:15 +0530

    adds home route

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,34 +5,58 @@ import 'package:derm_detect_app/screens/team_screen.dart';
 import 'package:derm_detect_app/widgets/login_register.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset(
-          'assets/nav_logo.png',
-          height: 36,
+        title: InkWell(
+          child: Image.asset(
+            'assets/nav_logo.png',
+            height: 36,
+          ),
+          onTap: () {
+            setState(() {
+              pageIndex = 0;
+            });
+          },
         ),
         centerTitle: false,
         actions: [
-          TextButton(onPressed: (){}, child: const Text('About')),
+          TextButton(onPressed: () {
+            setState(() {
+              pageIndex = 1;
+            });
+          }, child: const Text('About')),
           space16,
-          TextButton(onPressed: (){}, child: const Text('Team')),
+          TextButton(onPressed: () {
+            setState(() {
+              pageIndex = 2;
+            });
+          }, child: const Text('Team')),
           space16,
-          TextButton(onPressed: (){}, child: const Text('Contact')),
+          TextButton(onPressed: () {}, child: const Text('Contact')),
           space16,
-          FilledButton(onPressed: (){
-            showDialog(context: context, builder: (context)=> const LoginRegisterDialog());
-          }, child: const Text('LOGIN'))
+          FilledButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => const LoginRegisterDialog());
+              },
+              child: const Text('LOGIN'))
         ],
       ),
-      body: const IndexedStack(
-        index: 2,
-        children: [IntroScreen(),AboutScreen(),TeamScreen()]
-      ),
+      body: IndexedStack(
+          index: pageIndex,
+          children: const [IntroScreen(), AboutScreen(), TeamScreen()]),
     );
   }
 }
